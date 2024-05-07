413c9a095b10b5c25a9c15e42e5324e69c1f5bbf
Sarah <154954522+sarahh1201@users.noreply.github.com>
2024-05-06 20:11:30 -0700

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -3,77 +3,99 @@
 #include <string>
 using namespace std; 
 
-double add(int num1, int num2);
-double subtract(int num1, int num2);
-double multiply(int num1, int num2);
-double divide(int num1, int num2);
-double power(int num1, int num2);
+double add(double num1, double num2);
+double subtract(double num1, double num2);
+double multiply(double num1, double num2);
+double divide(double num1, double num2);
+double power(double num1, double num2);
+
+void clearScreen() //Clears the screen
+{
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+}
 
 int main()
 {
     double num1, num2;
+    int ext=1; //If equal to zero the program will end
     char op;
+while (ext==1) //Exit must be 0 to exit the program
+{
+    clearScreen();
+    cout <<"------------------\n|   CALCULATOR   |\n------------------\n";
     cin>>num1>>op>>num2;
-
-    switch (op)
-    {
-        case '+':
+    
+        switch (op)
         {
-            cout << add(num1,num2);
-            break;
+            case '+':
+            {
+                cout <<" = " << add(num1,num2)<<"\n";
+                cout << "Press 1 to continue or 0 to exit: "; cin>>ext;
+                break;
+            }
+            case '-':
+            {
+                cout << "= " << subtract(num1,num2)<<"\n";
+                cout << "Press 1 to continue or 0 to exit: "; cin>>ext;
+                break;
+            }
+            case '*':
+            {
+                cout << "= " << multiply(num1,num2)<<"\n";
+                cout << "Press 1 to continue or 0 to exit: "; cin>>ext;
+                break;
+            }
+            case '/':
+            {
+                cout << "= " << divide(num1,num2)<<"\n";
+                cout << "Press 1 to continue or 0 to exit: "; cin>>ext;
+                break;
+            }   
+            case '^':
+            {
+                cout << "= " << power(num1,num2)<<"\n";
+                cout << "Press 1 to continue or 0 to exit: "; cin>>ext;
+                break;
+            } 
+            default:
+            {
+                cout << "Invalid operator. Please try again!\n";
+                cout << "Press 1 to continue or 0 to exit: "; cin>>ext;
+                break;
+            }
         }
-        case '-':
-        {
-            cout << subtract(num1,num2);
-            break;
-        }
-        case '*':
-        {
-            cout << multiply(num1,num2);
-            break;
-        }
-        case '/':
-        {
-            cout << divide(num1,num2);
-            break;
-        }   
-        case '^':
-        {
-            cout << power(num1,num2);
-            break;
-        } 
-        default:
-        {
-            cout << "Invalid operator. Please try again!";
-            break;
-        }
-    }
-
+}
+clearScreen();
+cout << "Exiting Calculator...";
 
     return 0;
 }
 
-double add(int num1, int num2)
+double add(double num1, double num2)
 {
     return num1+num2;
 }
 
-double subtract(int num1, int num2)
+double subtract(double num1, double num2)
 {
     return num1-num2;
 }
 
-double multiply(int num1, int num2)
+double multiply(double num1, double num2)
 {
     return num1*num2;
 }
 
-double divide(int num1, int num2)
+double divide(double num1, double num2)
 {
     return num1/num2;
 }
 
-double power(int num1, int num2)
+double power(double num1, double num2)
 {
     return pow(num1,num2);
 }
